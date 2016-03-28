e6f8bcaad9eb27bde65fe7bf8c28acbba90e4725
Piotr Kwiatkowski <p.kwiatkowski@zoho.com>
2016-03-28 16:30:41 +0200

    timing functions comparison/tfc.py: results and plots save to files, test ready to run

--- timing functions comparison/tfc.py
+++ timing functions comparison/tfc.py
@@ -27,12 +27,13 @@ def _quicksort(l, start, stop):
         _quicksort(l, left, stop)
 
 
-seq_lengths = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000]
+seq_lengths = [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000,
+               1000000, 5000000, 10000000]
 
 
 def wrapper(f, l):
     print (f.__name__ + ' %d') % l
-    return f(quicksort, gen_seq_complete_random(l))
+    return f(quicksort, gen_seq_complete_random(l), 100)
 
 d_time_algo_min = [wrapper(time_algo_min, l) for l in seq_lengths]
 d_time_algo_mean = [wrapper(time_algo_mean, l) for l in seq_lengths]
@@ -40,6 +41,15 @@ d_time_algo_included_min = [wrapper(time_algo_included_min, l)
                             for l in seq_lengths]
 d_time_algo_included_mean = [wrapper(time_algo_included_mean, l)
                              for l in seq_lengths]
+
+with open('result.txt', 'w') as f:
+    f.write('seq_lengths: ' + str(seq_lengths) + '\n')
+    f.write('d_time_algo_min: ' + str(d_time_algo_min) + '\n')
+    f.write('d_time_algo_mean: ' + str(d_time_algo_mean) + '\n')
+    f.write('d_time_algo_included_min: ' +
+            str(d_time_algo_included_min) + '\n')
+    f.write('d_time_algo_included_mean: ' +
+            str(d_time_algo_included_mean) + '\n')
 
 plt.plot(seq_lengths, d_time_algo_min,
          '--', marker='x', label='min')
@@ -49,16 +59,12 @@ plt.plot(seq_lengths, d_time_algo_included_min,
          '--', marker='x', label='included_min')
 plt.plot(seq_lengths, d_time_algo_included_mean,
          '--', marker='x',  label='included_mean')
-plt.xscale('log')
-plt.yscale('log')
+
 plt.grid(True)
 plt.xlabel('sequence length')
 plt.ylabel('time (s)')
 plt.legend(bbox_to_anchor=(0, 1), loc=2, borderaxespad=0., frameon=False)
-plt.show()
-
-#x = range(len(seq_lengths))
-#plt.yaxis('log')
-#plt.bar(x, d_time_algo_min)
-
-#plt.show()
+plt.savefig('plot_linear.png',  bbox_inches='tight')
+plt.xscale('log')
+plt.yscale('log')
+plt.savefig('plot_log.png',  bbox_inches='tight')
